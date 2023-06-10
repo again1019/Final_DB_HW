1a02140a07c69bac1be4197463bcf89f928a6d51
AuTong <D1093068@o365.fcu.edu.tw>
2023-06-11 05:49:34 +0800

    modify

--- bplus_tree/csvblock/test_bptree.cpp
+++ bplus_tree/csvblock/test_bptree.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int bucketSize = 10;
+int bucketSize = 5000;
 
-// Create 2 classes, one for node and one for tree;
+// Create 2 classes, one for node and one for bptree;
 
 class node {
 public:
@@ -20,10 +20,10 @@ public:
     }
 };
 
-class Btree {
+class bptree {
 public:
 	node* root; // Root of tree stored here;
-	Btree() {
+	bptree() {
         root = NULL;
     }
 
@@ -38,24 +38,24 @@ public:
 };
 
 
-void Btree::insert(string x) {
+void bptree::insert(string x) {
     // insert a new node into the tree
 	if (root == NULL) {
 		root = new node;
 		root->key[0] = x;
 		root->isLeaf = true;
 		root->size = 1;
-	}
-	else {
+	} else {
 		node* current = root;
 		node* parent;
         
         // if current node is not a leaf, then go to the leaf
 		while (!current->isLeaf) {
 			parent = current;
-
+            // | < a | A | a <= x < b | B | >= b | 
+            // |  i  | i |     i+1    |i+1| i+2  | 
 			for (int i = 0; i < current->size; i++) {
-				if (x.id < (current->key[i]).id) {
+				if (x.compare(current->key[i]) < 0) {
 					current = current->ptr[i];
 					break;
 				}
@@ -66,74 +66,66 @@ void Btree::insert(string x) {
 			}
 		}
 
-		// now we have reached leaf;
-		if (current->size
-			< bucketSize) { // if the node to be inserted is
-							// not filled
+		// reached leaf;
+		if (current->size < bucketSize) { // if the node to be inserted is not filled
 			int i = 0;
 
-			// Traverse btree
-			while (x.id > (current->key[i]).id && i < current->size)
-				// goto pt where needs to be inserted.
+			// find the right position of element to be inserted
+			while (x.compare(current->key[i]) > 0 && i < current->size)
+				// go to ptr where needs to be inserted.
 				i++;
-
 			for (int j = current->size; j > i; j--)
-				// adjust and insert element;
+				// adjust the elements to make space for new element
 				current->key[j] = current->key[j - 1];
-
 			current->key[i] = x;
 
 			// size should be increased by 1
 			current->size++;
-
-			current->ptr[current->size]
-				= current->ptr[current->size - 1];
+             
+            // ptr in leaf just need the end ptr to next leaf, other is null
+			current->ptr[current->size] = current->ptr[current->size - 1];
 			current->ptr[current->size - 1] = NULL;
-		}
-
-		// if block does not have enough space;
-		else {
+		} else { // if block does not have enough space;
 			node* newLeaf = new node;
-			cid tempNode[bucketSize + 1];
+			string tempNode[bucketSize + 1]; // in order to copy the elements
 
-			for (int i = 0; i < bucketSize; i++)
-				// all elements of this block stored
+			for (int i = 0; i < bucketSize; i++) {
+				// all elements of this block copy
 				tempNode[i] = current->key[i];
+            }
 			int i = 0, j;
 
-			// find the right posn of num to be inserted
-			while (x.id > tempNode[i].id && i < bucketSize)
+			// find the right position of element to be inserted
+			while (x.compare(current->key[i]) > 0 && i < bucketSize)
 				i++;
-
 			for (int j = bucketSize + 1; j > i; j--)
+				// adjust the elements to make space for new element
+				// current->key[j] = current->key[j - 1];
 				tempNode[j] = tempNode[j - 1];
 			tempNode[i] = x;
 			// inserted element in its rightful position;
 
 			newLeaf->isLeaf = true;
 			current->size = (bucketSize + 1) / 2;
-			newLeaf->size
-				= (bucketSize + 1)
-				- (bucketSize + 1)
-						/ 2; // now rearrangement begins!
+			newLeaf->size = (bucketSize + 1) - (bucketSize + 1) / 2; 
 
-			current->ptr[current->size] = newLeaf;
-			newLeaf->ptr[newLeaf->size]
-				= current->ptr[bucketSize];
+            
+            // ================= rerangement new leaf =================
+			current->ptr[current->size] = newLeaf; // old end ptr is new (be divided size)
+			newLeaf->ptr[newLeaf->size] = current->ptr[bucketSize]; // new end ptr is old's old size end (not be divided size)
 
-			current->ptr[newLeaf->size]
-				= current->ptr[bucketSize];
+			current->ptr[newLeaf->size] = current->ptr[bucketSize]; // if the size different, then the ptr is not the same
 			current->ptr[bucketSize] = NULL;
 
 			for (int i = 0; i < current->size; i++)
 				current->key[i] = tempNode[i];
 
-			for (int i = 0, j = current->size;
-				i < newLeaf->size; i++, j++)
+			for (int i = 0, j = current->size; i < newLeaf->size; i++, j++)
 				newLeaf->key[i] = tempNode[j];
 
-			// if this is root, then fine,
-			// else we need to increase the height of tree;
+
+			// if this is root, create a new root;
+			// else need to increase the height of tree;
 			if (current == root) {
 				node* newRoot = new node;
 				newRoot->key[0] = newLeaf->key[0];
@@ -142,69 +134,61 @@ void Btree::insert(string x) {
 				newRoot->isLeaf = false;
 				newRoot->size = 1;
 				root = newRoot;
-			}
-			else
-				shiftLevel(
-					newLeaf->key[0], parent,
-					newLeaf); // parent->original root
+			} else {
+				shiftLevel(newLeaf->key[0], parent, newLeaf);
+            }
 		}
 	}
 }
 
-void Btree::shiftLevel(cid x, node* current, node* child)
-{ // insert or create an internal node;
-	if (current->size
-		< bucketSize) { // if can fit in this level, do that
+void bptree::shiftLevel(string x, node* current, node* child) { 
+    // insert or create an internal node;
+	if (current->size < bucketSize) { // if it is not full
 		int i = 0;
-		while (x.id > current->key[i].id && i < current->size)
+		while (x.compare(current->key[i]) > 0 && i < current->size)
 			i++;
-		for (int j = current->size; j > i; j--)
+		for (int j = current->size; j > i; j--) {
 			current->key[j] = current->key[j - 1];
-
-		for (int j = current->size + 1; j > i + 1; j--)
+        }
+		for (int j = current->size + 1; j > i + 1; j--) {
 			current->ptr[j] = current->ptr[j - 1];
-
+        }
 		current->key[i] = x;
 		current->size++;
 		current->ptr[i + 1] = child;
-	}
-
-	// shift up
-	else {
+	} else { 
+        // shift up
 		node* newInternal = new node;
-		cid tempKey[bucketSize + 1];
+		string tempKey[bucketSize + 1];
 		node* tempPtr[bucketSize + 2];
 
+        // copy to temp
 		for (int i = 0; i < bucketSize; i++)
 			tempKey[i] = current->key[i];
-
 		for (int i = 0; i < bucketSize + 1; i++)
 			tempPtr[i] = current->ptr[i];
 
 		int i = 0, j;
-		while (x.id > tempKey[i].id && i < bucketSize)
+		while (x.compare(current->key[i]) > 0 && i < bucketSize)
 			i++;
 
 		for (int j = bucketSize + 1; j > i; j--)
 			tempKey[j] = tempKey[j - 1];
-
 		tempKey[i] = x;
+        
+
 		for (int j = bucketSize + 2; j > i + 1; j--)
 			tempPtr[j] = tempPtr[j - 1];
-
 		tempPtr[i + 1] = child;
 		newInternal->isLeaf = false;
 		current->size = (bucketSize + 1) / 2;
 
-		newInternal->size
-			= bucketSize - (bucketSize + 1) / 2;
+		newInternal->size = bucketSize - (bucketSize + 1) / 2;
 
-		for (int i = 0, j = current->size + 1;
-			i < newInternal->size; i++, j++)
+		for (int i = 0, j = current->size + 1; i < newInternal->size; i++, j++)
 			newInternal->key[i] = tempKey[j];
 
-		for (int i = 0, j = current->size + 1;
-			i < newInternal->size + 1; i++, j++)
+		for (int i = 0, j = current->size + 1; i < newInternal->size + 1; i++, j++)
 			newInternal->ptr[i] = tempPtr[j];
 
 		if (current == root) {
@@ -217,26 +201,23 @@ void Btree::shiftLevel(cid x, node* current, node* child)
 			root = newRoot;
 		}
 
-		else
-			shiftLevel(current->key[current->size],
-					findParent(root, current),
-					newInternal);
+		else {
+            shiftLevel(current->key[current->size],findParent(root, current), newInternal);
+        }
 	}
 }
-int Btree::search(int x)
-{
+string bptree::search(string x) {
 	if (root == NULL)
-		return -1;
-
+		return NULL;
 	else {
 		node* current = root;
+        // find the leaf node
 		while (current->isLeaf == false) {
 			for (int i = 0; i < current->size; i++) {
-				if (x < current->key[i].id) {
+				if (x.compare(current->key[i]) < 0) {
 					current = current->ptr[i];
 					break;
 				}
-
 				if (i == current->size - 1) {
 					current = current->ptr[i + 1];
 					break;
@@ -244,54 +225,11 @@ int Btree::search(int x)
 			}
 		}
 
-		for (int i = 0; i < current->size; i++) {
-			if (current->key[i].id == x) {
-				cout<<"Course: "<< current->key[i].id <<endl;
-                for (int k = 0; k < current->key[i].student.size(); k++)
-                    cout << current->key[i].student[k] << " ";
-				return 1;
-				// return;
-			}
-		}
-
-		// cout<<"Key not found"<<endl;
-		return 0;
+		return current->key[0];
 	}
 }
 
-/*
-// Print the tree
-void Btree::display(node* current)
-{
-	if (current == NULL)
-		return;
-	queue<node*> q;
-	q.push(current);
-	while (!q.empty()) {
-		int l;
-		l = q.size();
-
-		for (int i = 0; i < l; i++) {
-			node* tNode = q.front();
-			q.pop();
-
-			for (int j = 0; j < tNode->size; j++)
-				if (tNode != NULL)
-					cout << tNode->key[j] << " ";
-
-			for (int j = 0; j < tNode->size + 1; j++)
-				if (tNode->ptr[j] != NULL)
-					q.push(tNode->ptr[j]);
-
-			cout << "\t";
-		}
-		cout << endl;
-	}
-}
-*/
-
-node* Btree::findParent(node* current, node* child)
-{
+node* bptree::findParent(node* current, node* child) {
 	node* parent;
 	if (current->isLeaf || (current->ptr[0])->isLeaf)
 		return NULL;
@@ -310,48 +248,25 @@ node* Btree::findParent(node* current, node* child)
 	return parent;
 }
 
-int main()
-{
-	Btree node;
+int main() {
+	bptree sid;
+    bptree cid;
 
+    // read file - sid
     FILE *fp;
-    fp = fopen("20_cid.csv", "r");
-
-    int lastid=1274;
-    cid course;
-
-    int count = 1;
-
-    while(!feof(fp)){
-        char str[20];
-        int id;
-        fscanf(fp, "%d,%s", &id, str);
-
-        //cout << count++ << endl;
-        if (id == lastid){
-            course.student.push_back(str);
-         //   cout << course.student[0] << "\n";
-
-        }
-        else{
-            course.id = lastid;
-        //    cout << course.id << " ";
-        //    for (int i = 0; i < course.student.size(); i++)
-        //        cout << course.student[i] << " ";
-            node.insert(course);
-            course.student.clear();
-            course.student.push_back(str);
-        }
-        lastid = id;
+    fp = fopen("SID.txt", "r");
+    while (!feof(fp)) {
+        char temp[11];
+        fscanf(fp, "%s", temp);
+        cout << temp << endl;
+        sid.insert(temp);
     }
-    course.id = lastid;
-    course.student.pop_back();
-    node.insert(course);
-    course.student.clear();
 
-    fclose(fp);
 
-    node.search(2142);
+    string input;
+    cin >> input;
+
+
 	return 0;
 }
 
